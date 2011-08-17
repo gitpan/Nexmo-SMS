cb9c14c2af17aa239daf804c5a239f432b409b1d
Renee Baecker <cpan@perl-services.de>
2011-08-17 08:36:33 +0000

    Import of RENEEB/Nexmo-SMS-0.01 from CPAN.
    gitpan-cpan-distribution: Nexmo-SMS
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         RENEEB/Nexmo-SMS-0.01.tar.gz
    gitpan-cpan-author:       RENEEB
    gitpan-cpan-maturity:     released

---
+++ lib/Nexmo/SMS.pm
@@ -0,0 +1,264 @@
+package Nexmo::SMS;
+
+use warnings;
+use strict;
+
+use Nexmo::SMS::TextMessage;
+
+
+our $VERSION = '0.01';
+
+
+
+my @attrs = qw(server username password);;
+
+for my $attr ( @attrs ) {
+    no strict 'refs';
+    *{ __PACKAGE__ . '::' . $attr } = sub {
+        my ($self,$value) = @_;
+        
+        my $key = '__' . $attr . '__';
+        $self->{$key} = $value if @_ == 2;
+        return $self->{$key};
+    };
+}
+
+sub new {
+    my ($class,%param) = @_;
+    
+    my $self = bless {}, $class;
+    
+    for my $attr ( @attrs ) {
+        if ( exists $param{$attr} ) {
+            $self->$attr( $param{$attr} );
+        }
+    }
+    
+    return $self;
+}
+
+
+sub sms {
+    my ($self,%param) = @_;
+    
+    my %types = (
+        text   => 'Nexmo::SMS::TextMessage',
+        #binary => 'Nexmo::SMS::BinaryMessage',
+    );
+    
+    my $requested_type = $param{type};
+    if ( exists $param{type} and !$types{$requested_type} ) {
+        $self->errstr("Type $requested_type not supported (yet)!");
+        return;
+    }
+        
+    my $type   = $requested_type || 'text';
+    my $module = $types{$type};
+    
+    # check for needed params
+    my $sub_name  = 'check_needed_params';
+    my $check_sub = $module->can( $sub_name );
+    if ( !$check_sub ) {
+        $self->errstr("$module does not know about sub $sub_name");
+        return;
+    }
+    
+    $param{server}   ||= $self->server;
+    $param{username} ||= $self->username;
+    $param{password} ||= $self->password;
+    
+    my $params_not_ok = $module->$sub_name( %param );
+    if ( $params_not_ok ) {
+        $self->errstr("Check params $params_not_ok");
+        return;
+    }
+    
+    # create new message
+    my $message = $module->new( %param );
+    
+    # return message 
+    return $message;
+}
+
+
+sub errstr {
+    my ($self,$message) = @_;
+    
+    $self->{__errstr__} = $message if @_ == 2;
+    return $self->{__errstr__};
+}
+
+
+sub get_balance {
+    warn "not implemented yet\n";
+    return;
+}
+
+
+sub get_pricing {
+    warn "not implemented yet\n";
+    return;
+}
+
+
+1; # End of Nexmo::SMS
+
+__END__
+=pod
+
+=head1 NAME
+
+Nexmo::SMS
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+This module simplifies sending SMS through the Nexmo API.
+
+    use Nexmo::SMS;
+
+    my $nexmo = Nexmo::SMS->new(
+        server   => 'http://test.nexmo.com/sms/json',
+        username => 'testuser1',
+        password => 'testpasswd2',
+    );
+    
+    my $sms = $nexmo->sms(
+        text     => 'This is a test',
+        from     => 'Test02',
+        to       => '452312432',
+    ) or die $nexmo->errstr;
+    
+    my $response = $sms->send || die $sms->errstr;
+    
+    if ( $response->is_success ) {
+        print "SMS was sent...\n";
+    }
+
+=head1 NAME
+
+Nexmo::SMS - Module for the Nexmo SMS API!
+
+=head1 VERSION
+
+Version 0.01
+
+=head1 METHODS
+
+=head2 new
+
+create a new object
+
+    my $foo = Nexmo::SMS->new(
+        server   => 'http://test.nexmo.com/sms/json',
+        username => 'testuser1',
+        password => 'testpasswd2',
+    );
+
+Those parameters are optional and they are used as defaults for the message objects
+
+=head2 sms
+
+Create a new message object or returns C<undef>.
+
+    my $sms = $nexmo->sms(
+        text     => 'This is a test',
+        from     => 'Test02',
+        to       => '452312432',
+    ) or die $nexmo->errstr;
+
+=head2 errstr
+
+return the "last" error as string.
+
+    print $nexmo->errstr;
+
+=head2 get_balance
+
+Not implemented yet...
+
+=head2 get_pricing
+
+Not implemented yet...
+
+=head1 Attributes
+
+These attributes are available for C<Nexmo::SMS::TextMessage> objects. For each
+attribute there is a getter/setter:
+
+  $nexmo->server( 'servername' );
+  my $server = $nexmo->server;
+
+=over 4
+
+=item * password
+
+=item * server
+
+=item * username
+
+=back
+
+=head1 AUTHOR
+
+Renee Baecker, C<< <module at renee-baecker.de> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-nexmo-sms at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Nexmo-SMS>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Nexmo::SMS
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Nexmo-SMS>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Nexmo-SMS>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Nexmo-SMS>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Nexmo-SMS/>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2011 Renee Baecker.
+
+This program is released under the following license: artistic_2
+
+=head1 AUTHOR
+
+Renee Baecker <module@renee-baecker.de>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2011 by Renee Baecker.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0
+
+=cut
+
---
+++ lib/Nexmo/SMS/Response.pm
@@ -0,0 +1,223 @@
+package Nexmo::SMS::Response;
+
+use strict;
+use warnings;
+
+use Nexmo::SMS::Response::Message;
+
+use JSON::PP;
+
+
+our $VERSION = '0.01';
+
+# create getter/setter
+my @attrs = qw(json message_count status);
+
+for my $attr ( @attrs ) {
+    no strict 'refs';
+    *{ __PACKAGE__ . '::' . $attr } = sub {
+        my ($self,$value) = @_;
+        
+        my $key = '__' . $attr . '__';
+        $self->{$key} = $value if @_ == 2;
+        return $self->{$key};
+    };
+}
+
+
+sub new {
+    my ($class,%param) = @_;
+    
+    my $self = bless {}, $class;
+    
+    return $self if !$param{json};
+    
+    # decode json
+    my $coder = JSON::PP->new->utf8->pretty->allow_nonref;
+    my $perl  = $coder->decode( $param{json} );
+    
+    $self->message_count( $perl->{'message-count'} );
+    $self->status( 0 );
+    
+    # for each message create a new message object
+    for my $message ( @{ $perl->{messages} || [] } ) {
+        $self->_add_message(
+            Nexmo::SMS::Response::Message->new( %{$message || {}} )
+        );
+    }
+    
+    return $self;
+}
+
+
+sub messages {
+    my ($self) = @_;
+    
+    return @{ $self->{__messages__} || [] };
+}
+
+sub _add_message {
+    my ($self,$message) = @_;
+    
+    if ( @_ == 2 and $message->isa( 'Nexmo::SMS::Response::Message' ) ) {
+        push @{$self->{__messages__}}, $message;
+        if ( $message->status != 0 ) {
+            $self->status(1);
+            $self->errstr( $message->status_text . ' (' . $message->status_desc . ')' );
+        }
+    }
+}
+
+
+
+sub errstr {
+    my ($self,$message) = @_;
+    
+    $self->{__errstr__} = $message if @_ == 2;
+    return $self->{__errstr__};
+}
+
+
+sub is_success {
+    my ($self) = @_;
+    return !$self->status;
+}
+
+
+sub is_error {
+    my ($self) = @_;
+    return $self->status;
+}
+
+1;
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Nexmo::SMS::Response
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+This module represents a response from Nexmo.
+
+    use Nexmo::SMS::Response;
+
+    my $nexmo = Nexmo::SMS::Response->new(
+        json => '{
+            "message-count":"1",
+            "messages":[
+              {
+              "status":"4",
+              "message-id":"message001",
+              "client-ref":"Test001 - Reference",
+              "remaining-balance":"20.0",
+              "message-price":"0.05",
+              "error-text":""
+              }
+            ]
+        }',
+    );
+    
+    for my $message ( $response ) {
+        print $message->status;
+    }
+
+=head1 NAME
+
+Nexmo::SMS::Response - Module that represents a response from Nexmo SMS API!
+
+=head1 VERSION
+
+Version 0.01
+
+=head1 METHODS
+
+=head2 new
+
+create a new object
+
+    my $foo = Nexmo::SMS::Response->new(
+        json => '{
+            "message-count":"1",
+            "messages":[
+              {
+              "status":"4",
+              "message-id":"message001",
+              "client-ref":"Test001 - Reference",
+              "remaining-balance":"20.0",
+              "message-price":"0.05",
+              "error-text":""
+              }
+            ]
+        }',
+    );
+
+=head2 messages
+
+returns the list of messages included in the response. Each element is an
+object of L<Nexmo::SMS::Response::Message>.
+
+    my @messages = $response->messages;
+
+=head2 errstr
+
+return the "last" error as string.
+
+    print $response->errstr;
+
+=head2 is_success
+
+returns 1 if all messages have a status = 0, C<undef> otherwise.
+
+=head2 is_error
+
+Returns 1 if an error occured, 0 otherwise...
+
+=head1 ATTRIBUTES
+
+These attributes are available for C<Nexmo::SMS::TextMessage> objects:
+
+  $nexmo->status( 'status' );
+  my $status = $nexmo->status;
+
+=over 4
+
+=item * json
+
+=item * message_count
+
+=item * status
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2011 Renee Baecker.
+
+This program is released under the following license: artistic_2
+
+=cut
+
+=head1 AUTHOR
+
+Renee Baecker <module@renee-baecker.de>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2011 by Renee Baecker.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0
+
+=cut
+
